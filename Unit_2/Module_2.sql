5b43e29772b79e6a1bed8e8da9dc4e8187bd306a
BirdCode888 <birdcode.cali@gmail.com>
2026-01-18 11:19:45 -0500

    按类型分类的记录数

--- Unit_2/Module_2.sql
+++ Unit_2/Module_2.sql
@@ -1,3 +1,7 @@
 SELECT COUNT(DISTINCT id) AS 记录总数
 FROM aves_yunnan;
 
+SELECT basisofrecord AS 记录类型, COUNT(DISTINCT id) AS 记录数, 
+ROUND((COUNT(DISTINCT id) / 408816.0) * 100, 2) AS 百分比
+FROM aves_yunnan
+GROUP BY 记录类型 ORDER BY 记录数 DESC, 百分比;
