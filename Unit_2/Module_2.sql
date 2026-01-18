f508febd0524f0357fc8afc290459b83193cc8e7
BirdCode888 <birdcode.cali@gmail.com>
2026-01-18 14:22:13 -0500

    作业 - 从上一个任务创建的表格中选择一个物种，计算该物种记录中坐标不确定性大于 1000 米的百分比

--- Unit_2/Module_2.sql
+++ Unit_2/Module_2.sql
@@ -16,7 +16,7 @@ WHERE basisofrecord = 'HUMAN_OBSERVATION'
 GROUP BY 分类学范围 ORDER BY 记录数 DESC, 百分比;
 
 
----- ****************** 作业 *******************************************
+---- ************************** 作业 ************************************************
 
 ------ 按属名分类的人类观察记录计数（分类范围为属）
 
@@ -26,7 +26,7 @@ FROM aves_yunnan
 WHERE basisofrecord = 'HUMAN_OBSERVATION' AND taxonrank = 'GENUS'
 GROUP BY 属 ORDER BY 记录数 DESC, 百分比;
 
----- ******************************************************************
+---- ********************************************************************************
 
 -- 人类观测数据存在超过1000米的坐标不确定性
 SELECT COUNT(DISTINCT id) AS 记录数,
@@ -35,7 +35,7 @@ FROM aves_yunnan
 WHERE coordinateuncertaintyinmeters > 1000 AND basisofrecord = 'HUMAN_OBSERVATION';
 
 
----- ****************** 作业 *********************************************************
+---- ************************** 作业 *************************************************
 
 ------ 坐标不确定度超过1000米的人类观测分类范围
 SELECT taxonrank AS 分类学范围, COUNT(DISTINCT id) AS 记录数,
@@ -44,10 +44,10 @@ FROM aves_yunnan
 WHERE coordinateuncertaintyinmeters > 1000 AND basisofrecord = 'HUMAN_OBSERVATION'
 GROUP BY 分类学范围 ORDER BY 记录数 DESC, 百分比;
 
----- *********************************************************************************
+---- ********************************************************************************
 
 
----- ****************** 作业 **********************************************************
+---- ************************** 作业 *************************************************
 
 ------ 人类观测到的物种记录中，坐标不确定度大于1000米的物种。
 SELECT species AS 学名, 汉语 AS 汉语名, 英语 AS 英名, 西班牙语 AS 西班牙名, 
@@ -57,7 +57,35 @@ FROM aves_yunnan
 WHERE coordinateuncertaintyinmeters > 1000 AND basisofrecord = 'HUMAN_OBSERVATION'
 GROUP BY 学名, 汉语名, 英名, 西班牙名 ORDER BY 记录数 DESC, 百分比;
 
----- *********************************************************************************
+---- ********************************************************************************
+
+---- ************************** 作业 *************************************************
+
+---- 从上一个任务创建的表格中选择一个物种，计算该物种记录中坐标不确定性大于 1000 米的百分比。
+
+------ 人类观察到的孔雀属（Pavo muticus）物种记录总数的统计
+SELECT species AS 学名, COUNT(DISTINCT id) AS 记录数
+FROM aves_yunnan
+WHERE species = 'Pavo muticus' AND basisofrecord = 'HUMAN_OBSERVATION'
+GROUP BY 学名 ORDER BY 记录数;
+
+------ 人类观测到的孔雀属（Pavo muticus）物种中，坐标不确定性大于1000米的记录占该物种总记录的百分比
+SELECT species AS 学名, COUNT(DISTINCT id) AS 记录数,
+ROUND((COUNT(DISTINCT id) / 158.0) * 100, 2) AS 百分比
+FROM aves_yunnan
+WHERE coordinateuncertaintyinmeters > 1000 AND basisofrecord = 'HUMAN_OBSERVATION'
+AND species = 'Pavo muticus'
+GROUP BY 学名 ORDER BY 记录数, 百分比;
+
+---- ********************************************************************************
+
+
+
+
+
+
+
+
 
 
 
