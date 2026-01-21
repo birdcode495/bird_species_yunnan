36eb755bf875c4af462e2d1985ea5b48efab58ff
BirdCode888 <birdcode.cali@gmail.com>
2026-01-21 10:18:15 -0500

    清除1950年以前坐标不确定度大于1000米的记录

--- Unit_2/Module_4.sql
+++ Unit_2/Module_4.sql
@@ -33,5 +33,16 @@ MAX(coordinateuncertaintyinmeters) AS 最大值
 FROM aves_yunnan
 WHERE year < 1950 AND coordinateuncertaintyinmeters > 1000;
 
+------ 1950 年以前的记录在临时清理之前的数量
+SELECT COUNT(DISTINCT id) AS 记录数
+FROM aves_yunnan;
 
+------ 删除不适合分析的记录
+DELETE FROM aves_yunnan
+WHERE 
+year < 1950 AND coordinateuncertaintyinmeters > 1000;
+
+------ 1950 年前的记录数量（经过临时清理后)
+SELECT COUNT(DISTINCT id) AS 记录数
+FROM aves_yunnan;
 ---- ***************************** 作业结束 *******************************
