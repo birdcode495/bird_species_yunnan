ea4613e9dee16c691972d41ce29f57d0cbcc92e1
BirdCode888 <birdcode.cali@gmail.com>
2026-01-21 10:09:56 -0500

    计算1950年以前坐标不确定度大于1000米的记录的统计指标

--- Unit_2/Module_4.sql
+++ Unit_2/Module_4.sql
@@ -3,11 +3,11 @@
 ---- **************************** 作业 ***********************************
 
 ------ 不同日期坐标不确定性统计指标的回顾
-SELECT COUNT(DISTINCT id) AS records,
-AVG(coordinateuncertaintyinmeters) AS media,
-STDDEV(coordinateuncertaintyinmeters) AS desviacion,
-MIN(coordinateuncertaintyinmeters) AS minimum,
-MAX(coordinateuncertaintyinmeters) AS maximum
+SELECT COUNT(DISTINCT id) AS 记录数,
+AVG(coordinateuncertaintyinmeters) AS 统计均值,
+STDDEV(coordinateuncertaintyinmeters) AS 标准差,
+MIN(coordinateuncertaintyinmeters) AS 最小值,
+MAX(coordinateuncertaintyinmeters) AS 最大值
 FROM aves_yunnan
 WHERE year < 1900;
 
@@ -23,5 +23,15 @@ year < 1900;
 ------ 1900 年前的记录数量（经过临时清理后)
 SELECT COUNT(DISTINCT id) AS 记录数
 FROM aves_yunnan;
+
+------ 不同日期坐标不确定性统计指标的回顾
+SELECT COUNT(DISTINCT id) AS 记录数,
+AVG(coordinateuncertaintyinmeters) AS 统计均值,
+STDDEV(coordinateuncertaintyinmeters) AS 标准差,
+MIN(coordinateuncertaintyinmeters) AS 最小值,
+MAX(coordinateuncertaintyinmeters) AS 最大值
+FROM aves_yunnan
+WHERE year < 1950 AND coordinateuncertaintyinmeters > 1000;
+
 
 ---- ***************************** 作业结束 *******************************
